0d0bc0f02a8a52fcabba3abebed273ff2b965822
Duane David <duanedave17@gmail.com>
2022-11-15 05:58:18 -0800

    Add book.rb file with methods

--- src/app/book.rb
+++ src/app/book.rb
@@ -1,6 +1,8 @@
-require_relatie 'item'
+require_relative 'item'
 
 class Book < Item
+  attr_accessor :cover_state, :publisher
+
   def initialize (cover_state, publisher, publish_date)
     super(publish_date)
     @cover_state = cover_state
