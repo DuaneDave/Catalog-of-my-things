850d3f647f5a5a5fc1645ae20c9da76d453fdb46
Kehinde Onifade <onifkay@gmail.com>
2022-11-15 11:55:55 +0100

    Implement startup actions in main.rb

---
+++ src/app/main.rb
@@ -0,0 +1,22 @@
+def main
+    puts "Welcome to Catalog of My Things App!"
+
+    def options(option)
+        case option
+        when 1
+            list_all_books
+        when 2
+            list_all_music_album
+        when 3
+            list_all_games
+        when 4
+            list_all_movies
+        when 5
+            puts "Thank you for using our app! Have a nice day"
+        else
+            puts "Incorrect option, please try again"
+        end
+    end
+end
+
+main
