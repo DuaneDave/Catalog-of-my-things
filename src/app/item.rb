71225907b6322fbd4f6e3fddc3328db3536717ee
Duane David <duanedave17@gmail.com>
2022-11-15 02:44:20 -0800

    Add move to archive method

--- src/app/item.rb
+++ src/app/item.rb
@@ -2,20 +2,21 @@ require 'date'
 
 class Item
   attr_accessor :publish_date
-  attr_reader :id, :archived, :genre, :label, :author
+  attr_reader :id, :genre, :label, :author, :archived
 
-  def initialize(publish_date, archived: false)
+  def initialize(publish_date)
     @id = Random.rand(1..150)
     @publish_date = Date.parse(publish_date)
-    @archived = archived
+    @archived = false
   end
 
   def can_be_archived?
     (DateTime.now - publish_date).to_i >= 3650
   end
 
-  # def move_to_archive
-  # end
+  def move_to_archive
+    @archived = true if can_be_archived?
+  end
 end
 
 item = Item.new('2002/11/14')
