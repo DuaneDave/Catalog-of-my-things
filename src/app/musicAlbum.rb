4ad423eb12656897d2d465c2adcf2397eaafd75a
Kehinde Onifade <onifkay@gmail.com>
2022-11-15 20:13:56 +0100

    set up musicAlbum properties

--- src/app/musicAlbum.rb
+++ src/app/musicAlbum.rb
@@ -1,5 +1,14 @@
 require_relative 'item'
 
 class MusicAlbum < Item
-    
+    attr_reader :on_spotify
+
+    def initialize(on_spotify, publish_date)
+        super(publish_date)
+        @on_spotify = on_spotify
+    end
+
+    def can_be_archived?
+        super && @on_spotify = true
+    end
 end
