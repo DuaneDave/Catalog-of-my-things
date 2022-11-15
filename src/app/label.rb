9a65ec4fc480ffaffa90cc5a306df70ad1c55f8c
Duane David <duanedave17@gmail.com>
2022-11-15 03:40:24 -0800

    Add label.rb

---
+++ src/app/label.rb
@@ -0,0 +1,16 @@
+class Label
+  attr_accessor :items
+  attr_reader :id, :title, :color
+
+  def initialize(title, color)
+    @id = Random.rand(1..150)
+    @title = title
+    @color = color
+    @items = []
+  end
+
+  def add_item(item)
+    item.label = self
+    items << item
+  end
+end
