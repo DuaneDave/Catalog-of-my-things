151ecc9e2d3ad6401f7a376148121d99e99d42e5
Kehinde Onifade <onifkay@gmail.com>
2022-11-16 19:54:39 +0100

    Fix linters and correct linter file error

--- src/spec/music_spec.rb
+++ src/spec/music_spec.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require 'rspec'
 require_relative('./../../spec/spec_helper')
 require_relative './../app/music'
@@ -8,7 +6,6 @@ describe Music do
   context 'when initialized' do
     it 'Should create a new music instance' do
       music = Music.new('2021-10-01', true)
-      # expect(music.publish_date).to eq('2021-01-01')
       expect(music.on_spotify).to eq(true)
       expect(music.can_be_archived?).to be(false)
     end
