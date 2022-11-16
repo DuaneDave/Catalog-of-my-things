3f936a5cdd0f01aa6b1159048f4f271ad761b367
Kehinde Onifade <onifkay@gmail.com>
2022-11-16 09:38:01 +0100

    Add some options in main.rb

--- src/app/main.rb
+++ src/app/main.rb
@@ -21,8 +21,14 @@ def main
     case option
     when 1
       app.list_all_books
+    when 2
+      app.list_all_music_albums
+    when 4
+      app.list_all_genres
     when 5
       app.list_all_labels
+    when 8
+      app.add_music_album
     when 9
       app.add_book
     when 0
