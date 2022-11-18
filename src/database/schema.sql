f9008be3762196f399f9998bec63c1e26a3cc96a
usernameBakhtiyor <harhil46@gmail.com>
2022-11-18 11:14:39 +0500

    Update schema.sql

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -35,3 +35,21 @@ CREATE TABLE music (
   on_spotify BOOLEAN,
   FOREIGN KEY(id) REFERENCES item(id)
 );
+
+CREATE TABLE authors(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE games(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    publish_date DATE NOT NULL,
+    archived BOOLEAN,
+    multiplayer BOOLEAN,
+    last_played_at DATE NOT NULL,
+    games_id INT,
+    PRIMARY KEY(id),
+    CONSTRAINT fk_authors FOREIGN KEY(games_id) REFERENCES authors(id)
+);
